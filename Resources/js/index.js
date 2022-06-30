8de9388c929b33a68eb73e99183ac7d5cb999a0e
Te <smoochbanner@icloud.com>
2022-06-29 18:31:19 -0700

    added html css and js

---
+++ Resources/js/index.js
@@ -0,0 +1,6 @@
+// var apiKey =() ;
+
+// submit the form to fetch weather information
+// handle button click to fetch  weather information
+// fetch geolocation data (geocoding api)
+// fetch weather data (onecall)
