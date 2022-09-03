8edd37e7e6a4b6d387b240f8af895a375bfa10b0
Te <smoochbanner@icloud.com>
2022-09-03 15:38:55 -0700

    psuedo code added to JS

--- Resources/js/index.js
+++ Resources/js/index.js
@@ -5,3 +5,28 @@
 // fetch geolocation data (geocoding api)
 // fetch weather data (onecall)
 console.log("are you attached");
+
+// Given a weather dashboard 
+// with `FORM` inputs
+//When I `Search`  for a city
+// Then I am `presented` with `current` and `future` conditions for that city
+// and that city is added to the `search history`
+// 
+
+
+// Weather Dashboard 
+// with form inputs
+// a search function 
+// Show current and future weather for searched location
+// searched locations added to search history 
+// When cities are viewed 
+// present the city name, date and an icon representing 
+// the weather, conditions, temperature, the huimidity, the wind speed, and the UV index
+// When viewing the UV index
+// Present the color thay indicates whether the conditions are favorable, moderate or severe
+// 
+
+// when viewing future conditions for that city 
+//  i am then presented with the 5day forcast that displays the date , an icon of the weather conditions, the temperature, the wind speed, and the humidty 
+//  when i click on a city in the history 
+// then i am again presented with the conditions, future and past conditions
